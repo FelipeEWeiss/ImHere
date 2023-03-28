0f3645f1535cacfda448cf9dc6c3d062eec20980
FelipeWeiss <felipeew19@gmail.com>
2023-03-27 23:28:43 -0300

    improve: add button component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
+  const handleParticipantAdd = () => {
+    console.log("Adicionar");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -17,6 +21,15 @@ export function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleParticipantAdd}
+      >
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -23,5 +23,17 @@ export const styles = StyleSheet.create({
     backgroundColor: '#1F1E25',
     padding: 16,
     fontSize: 16,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#FDFCFE',
+    fontSize: 24
   }
 });
